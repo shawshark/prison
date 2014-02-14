eb238d7643d58fe66af68abc664cf9d0c9312153
Chris <chrissbolding@gmail.com>
2014-02-14 22:51:19 +1100

    Create groups.java

---
+++ src/me/shawshark/rankup/groups.java
@@ -0,0 +1,10 @@
+package me.shawshark.test;
+
+public class groups {
+	public String groupname;
+	public int price;
+	public String required;
+	public groups(String igroupname, int iprice, String irequired) { 
+		this.groupname = igroupname; this.price = iprice; this.required = irequired;
+	}
+}
