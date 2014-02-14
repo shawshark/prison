c54407625e8f9090683cf4aecf928987b1cd9ba3
Chris <chrissbolding@gmail.com>
2014-02-15 00:07:48 +1100

    Create Blocks.java

---
+++ src/me/shawshark/prison/Blocks.java
@@ -0,0 +1,15 @@
+package me.shawshark.prison;
+
+import org.bukkit.Location;
+
+public class Blocks {
+	
+	public Location loc;
+	public String id;
+	
+	public Blocks(Location iloc, String iid) {
+		this.loc = iloc;
+		this.id = iid;
+	}
+
+}
