b99062bbf09828d272545ed73aec4485aea0e56c
Chris <chrissbolding@gmail.com>
2014-02-14 23:31:18 +1100

    Update main.java

--- src/me/shawshark/prison/main.java
+++ src/me/shawshark/prison/main.java
@@ -3,42 +3,32 @@ package me.shawshark.prison;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class main extends JavaPlugin {
 	
 	public List<groups> groups = new ArrayList<groups>();
 	
+	public RankupCommand rankupcmd;
+	public loadgroups loadgroups;
+	
 	public void onEnable() {
+		
+		saveDefaultConfig();
+		
+		
+		
+		rankupcmd = new RankupCommand(this);
+		loadgroups = new loadgroups(this);
+		
+		
+		loadgroups.load();
 		
 	}
 	
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(command.getName().equalsIgnoreCase("rankup")) {
-			
-			if(sender instanceof Player) {
-				Player p = (Player) sender;
-					
-				if(args.length == 0) {
-					
-					p.sendMessage(ChatColor.GOLD + "You can rankup to the following groups!");
-					
-					for ( groups g : groups) {
-						String groups = g.groupname;
-						int price = g.price;
-						
-						p.sendMessage(ChatColor.GREEN + "- " + 
-								ChatColor.WHITE + groups + ChatColor.GOLD + "Price " + price);						
-					}
-					
-				}
-			}
-		}
-		return true;
+	public void loadCommands() {
+		
+		getCommand("rankup").setExecutor(rankupcmd);
+		System.out.println("<Prison> Loaded commands!");
 	}
 }
